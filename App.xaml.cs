9ba3601f5f5dabf1e6cc9c57b39853066eaa9597
LUIZ MARTINS <luiz.martins139@etec.sp.gov.br>
2026-08-23 19:39:02 -0300

    atualização agenda 3

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,17 +1,29 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MauiAppCompras.Helpers;
 
 namespace MauiAppCompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+        
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "banco_sqlite_compras.db3");
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+                return _db;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
-        }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
+            MainPage = new NavigationPage(new Views.ListaProduto());
         }
     }
 }
--- Views/NovoProduto.xaml.cs
+++ Views/NovoProduto.xaml.cs
@@ -1,3 +1,5 @@
+using MauiAppCompras.Models;
+
 namespace MauiAppCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +8,27 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Produto p = new Produto
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco.Text)
+			};
+
+			await App.Db.Insert(p);
+			await DisplayAlertAsync("Sucesso!", "Registro Inserido.", "OK");
+
+		}
+		catch ( Exception ex)
+		{
+
+			await DisplayAlertAsync("Ops", ex.Message, "OK");
+
+		}
+    }
 }
